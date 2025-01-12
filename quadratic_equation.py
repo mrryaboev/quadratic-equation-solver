4c99c6cf3ecfc34eaaf67744ce87dd31d399cfe9
mrryaboev <mr.ryaboev@bk.ru>
2025-01-12 22:33:28 +0300

    Update quadratic_equation.py

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -2,26 +2,24 @@ import cmath
 
 def solve_quadratic_equation(a, b, c):
     """
-    Решает квадратное уравнение вида ax^2 + bx + c = 0.
-
-    Args:
-        a: Коэффициент при x^2 (не должен быть равен 0).
-        b: Коэффициент при x.
-        c: Свободный член.
-
-    Returns:
-        Кортеж (tuple) с корнями (могут быть комплексными), либо None, если a = 0.
+    Решает квадратное уравнение ax^2 + bx + c = 0
+    Возвращает кортеж из двух корней (возможно комплексных).
     """
+
     if a == 0:
-        return None  # Если a = 0, то не квадратное уравнение
+      if b == 0:
+           if c == 0:
+               return "Бесконечное число решений"
+           else:
+                return "Нет решений"
+      else:
+        return -c/b, -c/b # Линейное уравнение
 
-    delta = b**2 - 4*a*c
-
+    delta = (b**2) - 4*(a*c)
     if delta >= 0:
-      x1 = (-b - delta**0.5) / (2*a)
-      x2 = (-b + delta**0.5) / (2*a)
+        x1 = (-b - delta**0.5) / (2*a)
+        x2 = (-b + delta**0.5) / (2*a)
     else:
-      x1 = (-b - cmath.sqrt(delta)) / (2*a)
-      x2 = (-b + cmath.sqrt(delta)) / (2*a)
-
+         x1 = (-b - cmath.sqrt(delta)) / (2 * a)
+         x2 = (-b + cmath.sqrt(delta)) / (2 * a)
     return x1, x2
