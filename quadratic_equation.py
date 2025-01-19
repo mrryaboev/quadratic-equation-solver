e5eb4aa166a5ddbaebfca1c935068536ffb50644
mrryaboev <mr.ryaboev@bk.ru>
2025-01-19 22:03:33 +0300

    Update quadratic_equation.py

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,25 +1,29 @@
+# quadratic_equation.py
+
 import cmath
 
 def solve_quadratic_equation(a, b, c):
     """
-    Решает квадратное уравнение ax^2 + bx + c = 0
-    Возвращает кортеж из двух корней (возможно комплексных).
-    """
+    Решает квадратное уравнение ax^2 + bx + c = 0.
 
+    Args:
+        a: Коэффициент при x^2.
+        b: Коэффициент при x.
+        c: Свободный член.
+
+    Returns:
+        Кортеж (x1, x2) с корнями уравнения.
+        Возвращает None, если a равно нулю (не квадратное уравнение).
+    """
     if a == 0:
-      if b == 0:
-           if c == 0:
-               return "Бесконечное число решений"
-           else:
-                return "Нет решений"
-      else:
-        return -c/b, -c/b # Линейное уравнение
+        return None  # Не квадратное уравнение
 
     delta = (b**2) - 4*(a*c)
+
     if delta >= 0:
         x1 = (-b - delta**0.5) / (2*a)
         x2 = (-b + delta**0.5) / (2*a)
     else:
-         x1 = (-b - cmath.sqrt(delta)) / (2 * a)
-         x2 = (-b + cmath.sqrt(delta)) / (2 * a)
+        x1 = (-b - cmath.sqrt(delta)) / (2 * a)
+        x2 = (-b + cmath.sqrt(delta)) / (2 * a)
     return x1, x2
