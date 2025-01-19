84ca77b34d13c52ccf82f228bfc7bdaf319af094
mrryaboev <mr.ryaboev@bk.ru>
2025-01-19 22:04:27 +0300

    Update test_quadratic_equation.py

--- test_quadratic_equation.py
+++ test_quadratic_equation.py
@@ -1,52 +1,45 @@
-import pytest
+# test_quadratic_equation.py
+
+import unittest
 from quadratic_equation import solve_quadratic_equation
+import cmath
 
-def test_two_real_roots():
-    x1, x2 = solve_quadratic_equation(1, -3, 2)
-    assert x1 == 1.0
-    assert x2 == 2.0
+class TestQuadraticEquation(unittest.TestCase):
 
-def test_one_real_root():
-    x1, x2 = solve_quadratic_equation(1, -4, 4)
-    assert x1 == 2.0
-    assert x2 == 2.0
+    def test_two_distinct_real_roots(self):
+        x1, x2 = solve_quadratic_equation(1, -5, 6)
+        self.assertAlmostEqual(x1, 2)
+        self.assertAlmostEqual(x2, 3)
 
-def test_complex_roots():
-    x1, x2 = solve_quadratic_equation(1, 2, 5)
-    assert x1 == (-1-2j)
-    assert x2 == (-1-2j)
+    def test_two_equal_real_roots(self):
+         x1, x2 = solve_quadratic_equation(1, -4, 4)
+         self.assertAlmostEqual(x1, 2)
+         self.assertAlmostEqual(x2, 2)
 
-def test_integer_coefficients():
-    x1, x2 = solve_quadratic_equation(2, -4, 2)
-    assert x1 == 1.0
-    assert x2 == 1.0
+    def test_complex_roots(self):
+        x1, x2 = solve_quadratic_equation(1, 1, 1)
+        self.assertAlmostEqual(x1, (-0.5-0.8660254037844386j))
+        self.assertAlmostEqual(x2, (-0.5+0.8660254037844386j))
 
-def test_float_coefficients():
-    x1, x2 = solve_quadratic_equation(0.5, -1.5, 1)
-    assert x1 == 1.0
-    assert x2 == 2.0
+    def test_b_equals_zero(self):
+        x1, x2 = solve_quadratic_equation(1, 0, -4)
+        self.assertAlmostEqual(x1, -2)
+        self.assertAlmostEqual(x2, 2)
 
-def test_a_equal_zero_no_solution():
-     assert solve_quadratic_equation(0, 0, 1) == 'Нет решений'
+    def test_c_equals_zero(self):
+        x1, x2 = solve_quadratic_equation(1, -5, 0)
+        self.assertAlmostEqual(x1, 0)
+        self.assertAlmostEqual(x2, 5)
 
-def test_a_equal_zero_inf_solutions():
-     assert solve_quadratic_equation(0, 0, 0) == 'Бесконечное число решений'
+    def test_a_equals_zero(self):
+        self.assertIsNone(solve_quadratic_equation(0, 2, 3))
 
-def test_a_equal_zero_linear():
-     x1, x2 = solve_quadratic_equation(0, 2, 2)
-     assert x1 == -1.0
-     assert x2 == -1.0
+    def test_returns_tuple(self):
+      result = solve_quadratic_equation(1, -5, 6)
+      self.assertIsInstance(result, tuple)
 
-def test_large_coefficients():
-    x1, x2 = solve_quadratic_equation(1000, -3000, 2000)
-    assert x1 == 1.0
-    assert x2 == 2.0
-    
-def test_small_coefficients():
-    x1, x2 = solve_quadratic_equation(0.001, -0.003, 0.002)
-    assert x1 == 1.0
-    assert x2 == 2.0
+    def test_returns_none_if_a_is_zero(self):
+        self.assertIsNone(solve_quadratic_equation(0, 5, 6))
 
-def test_invalid_input():
-    with pytest.raises(TypeError):
-        solve_quadratic_equation("a", "b", "c")
+if __name__ == '__main__':
+    unittest.main()
